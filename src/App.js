baf1598b53d45ebee7909fe77378a756682e02cd
PhongVo0206 <pdvo@ucsd.edu>
2020-12-14 02:23:18 -0800

    Dropdown completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 
 const items = [
@@ -18,9 +19,32 @@ const items = [
     }
 ]
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shae of Blue',
+        value: 'blue'
+    }
+]
+
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true)
     return (
-    <Search />
+        <div>
+            <button onClick= {() => {setShowDropdown(!showDropdown)}}>Toggle Dropdown</button>
+    { showDropdown ? 
+    <Dropdown selected = {selected}
+    onSelectedChange = {setSelected}
+    options = {options}/> : null}
+        </div>
     );
 };
 
